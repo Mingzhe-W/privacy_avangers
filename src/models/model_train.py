737dd16c10015ab901688bd030c2839e675b7e41
Mingzhe-W <mingzhe3@illinois.edu>
2024-05-04 22:46:40 +1000

    merge back and frontend, backdoor attack

--- src/models/model_train.py
+++ src/models/model_train.py
@@ -5,11 +5,14 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data(type='digits')
 
 
+# flatten and normalize
 x_train = x_train.reshape((x_train.shape[0], -1)).astype("float32") / 255.0
 x_test = x_test.reshape((x_test.shape[0], -1)).astype("float32") / 255.0
+
 
 
 x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=42)
@@ -31,3 +34,4 @@ history = model.fit(x_train, y_train, epochs=10, batch_size=128, validation_data
 
 test_loss, test_acc = model.evaluate(x_test, y_test)
 print(f'Test accuracy: {test_acc}')
+
