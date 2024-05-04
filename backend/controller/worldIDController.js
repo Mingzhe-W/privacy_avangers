b6b4c27f3115fe4fd98c7bf0bc978b928b8cb597
QIXUAN LIN <linq0016@e.ntu.edu.sg>
2024-05-05 02:27:00 +1000

    Wordcoin update

--- backend/controller/worldIDController.js
+++ backend/controller/worldIDController.js
@@ -1,23 +1,43 @@
 import worldIDService from '../service/worldIDService.js';
 
+
 export const verifyWorldID = async (req, res, next) => {
   try {
-    let proof = req.body.proof
-    let ret = await worldIDService.verifyWorldID(proof)
+    console.log('req.body', req.body);
+    let proof = JSON.parse(req.body.proof);
+    //console.log('proof', proof)
+    let ret = await worldIDService.verifyWorldID(proof,res);
 
-    console.log('controller', ret)
+  //   console.log('controller', ret);
+  //   ret.then((response) => {
+  //     console.log('response', response);
+  //     response.then((wldResponse) => {
+  //     console.log('wldresponse', wldResponse);
+  //     if (response.status == 200) {
+  //       res.status(response.status).send({
+  //         code: "success",
+  //         detail: "This action verified correctly!",
+  //       });
+  //     }else {
+  //       res
+  //         .status(response.status)
+  //         .send({ code: wldResponse.code, detail: wldResponse.detail });
+  //     }
+  //     });
+  //   });
+  
 
-    if (ret.success) {
-      // let result = {
-      //   data: ret,
-      // }
-      // res.status(200).send(result);
-      res.status(200).send(ret);
-    } else {
-      res.status(400).send(ret);
-    }
+  // //   if (ret.success) {
+  // //     // let result = {
+  // //     //   data: ret,
+  // //     // }
+  // //     // res.status(200).send(result);
+  // //     res.status(200).send(ret);
+  // //   } else {
+  // //     res.status(400).send(ret);
+  // //   }
   } catch (err) {
     next(err)
   }
-};
+}
 
